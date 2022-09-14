df56efb65ec1dd671ffae05678fc97aee8bb4736
Antonio Garcia de Souza Neto <antonio.souza@compasso.com.br>
2022-09-14 20:25:27 -0300

    init structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,24 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
   ],
+
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+
   overrides: [
     {
       files: [
@@ -24,5 +29,19 @@ module.exports = {
         jest: true,
       },
     },
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
   ],
 };
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
